04e931b406f69db2d016b3fab606e395d6d74326
DatalinkWeb <harisamarad@datalink.com.gr>
2018-09-17 13:36:12 +0300

    listing

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,2 +1,9 @@
-class Floor < ApplicationRecord
+class Floor < ApplicationRecord::Base
+    def poster
+        "https://www.connexion.gr/retaliana/#{poster_url}"
+    end
+
+    def details
+        "https://www.connexion.gr/retaliana/details.html"
+    end
 end
